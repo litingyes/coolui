dc47d4e1c88f957f7b67c9c52f792cf517295fab
liting <luz.liting@gmail.com>
2024-09-20 21:04:03 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wow UI
+
+Opinionated and lightweight ui library, inspired by [Onu UI](https://onu.zyob.top/)
---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  unocss: true,
+  ignores: [
+    'pnpm-lock.yaml',
+  ],
+})
