c8ccd06c582f0f707221d8c5b242c793bf61fb27
liting <luz.liting@gmail.com>
2024-09-21 13:29:05 +0800

    fix(preset): set type for theme color gen

--- packages/preset/src/theme.ts
+++ packages/preset/src/theme.ts
@@ -3,12 +3,24 @@ import type { PresetWowOptions } from './index'
 import { theme } from 'magic-color'
 
 export function createTheme(options: PresetWowOptions): Theme {
-  const primaryColors = theme(options.theme.primary)
-  const coolColors = theme(options.theme.cool)
-  const infoColors = theme(options.theme.info)
-  const successColors = theme(options.theme.success)
-  const warningColors = theme(options.theme.warning)
-  const errorColors = theme(options.theme.error)
+  const primaryColors = theme(options.theme.primary, {
+    type: 'rgb',
+  })
+  const coolColors = theme(options.theme.cool, {
+    type: 'rgb',
+  })
+  const infoColors = theme(options.theme.info, {
+    type: 'rgb',
+  })
+  const successColors = theme(options.theme.success, {
+    type: 'rgb',
+  })
+  const warningColors = theme(options.theme.warning, {
+    type: 'rgb',
+  })
+  const errorColors = theme(options.theme.error, {
+    type: 'rgb',
+  })
 
   return {
     colors: {
